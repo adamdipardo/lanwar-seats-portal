fa60f6362ddf826b75b6436a829fbb6c3d4be9a2
Adam Di Pardo <adamdipardo@gmail.com>
2017-09-21 15:26:50 -0400

    style adjustments

--- js/components/RoomsList.js
+++ js/components/RoomsList.js
@@ -69,7 +69,7 @@ var RoomsList = React.createClass({
 		var options = [];
 		var disabledRooms = [84,85,91];
 		Object.keys(rooms).map(function(id) {
-			if (this.props.isAdminView == true || disabledRooms.indexOf(rooms[id].id) == -1)
+			if ((this.props.isAdminView == true || window.location.href.indexOf("/admin/orders/") > -1) || disabledRooms.indexOf(rooms[id].id) == -1)
 				options.push({value: String(id), label: rooms[id].name + "   (" + rooms[id].seats.taken + "/" + rooms[id].seats.total + ")"});
 		}.bind(this));
 
--- js/components/TableRow.js
+++ js/components/TableRow.js
@@ -13,10 +13,25 @@ var TableRow = React.createClass({
         var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         var seats = [];
+        var seatNumbers = [];
+        var seatNumbersCount = 0;
+        var seatNumbersNoSeatCount = 0;
         for (var i = 0; i < this.props.numColumns; i++) {
 
-            if (this.props.columnSplits.indexOf(alphabet.charAt(i)) >= 0)
+            if (this.props.columnSplits.indexOf(alphabet.charAt(i)) >= 0) {
                 seats.push(<td className="separator"></td>);
+            }
+
+            if (this.props.columnSplits.indexOf(alphabet.charAt(i)) >= 0 || i == this.props.numColumns - 1) {
+
+                if (seatNumbersCount > 0)
+                    seatNumbers.push(<td colSpan={seatNumbersCount} className="seat-count">{seatNumbersCount} Seats</td>);
+
+                seatNumbers.push(<td className="separator" colSpan={seatNumbersNoSeatCount + 1}></td>);
+
+                seatNumbersNoSeatCount = 0;
+                seatNumbersCount = 0;
+            }
 
             var foundSeat = false;
             for (var x = 0; x < row.seats.length; x++) {
@@ -35,6 +50,7 @@ var TableRow = React.createClass({
                     }
 
                     seats.push(<td className={classNames(classes)}>{label}</td>);
+                    seatNumbersCount++;
                     break;
                 }
 
@@ -42,16 +58,22 @@ var TableRow = React.createClass({
 
             if (!foundSeat) {
                 seats.push(<td className="no-seat"></td>);
+                seatNumbersNoSeatCount++;
             }
 
         }
 
         return (
-            <tr>
-                <td>{row.name}</td>
-                {seats}
-                <td>{row.name}</td>
-            </tr>
+            <tbody>
+                <tr>
+                    <td rowSpan="2">{row.name}</td>
+                    {seats}
+                    <td rowSpan="2">{row.name}</td>
+                </tr>
+                <tr>
+                    {seatNumbers}
+                </tr>
+            </tbody>
         );
 
 
